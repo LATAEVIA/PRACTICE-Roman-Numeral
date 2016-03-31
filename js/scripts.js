90beacdfc184620b5a8af6945ad7baa12bfa4a77
LaTaevia Berry and Sam Durfey <lataevia@>
2016-03-31 10:59:48 -0700

    Will now work for all numbers possible in roman numerals. Also, spits out an error if the number is not possible, ie, too high.

--- js/scripts.js
+++ js/scripts.js
@@ -17,8 +17,27 @@ $(document).ready(function(){
     event.preventDefault();
 
     userInput = $("#text-input").val();
-    $("#output").text(numeralCodes[0][userInput]);
-    // this will work for entering a one-digit number, 0-9.
+    console.log(typeof userInput);
+    // this will work for entering a one-digit number, 0-9:
+    // $("#output").text(numeralCodes[0][userInput]);
+
+    // and this one will work for a two-digit number, but break if it's one or three digits:
+    // $("#output").text(numeralCodes[1][userInput[0]] + numeralCodes[0][userInput[1]]);
+    // three-digit solver:
+    // $("#output").text(numeralCodes[2][userInput[0]] + numeralCodes[1][userInput[1]] + numeralCodes[0][userInput[2]]);
+
+    if (userInput > 3999) {
+      $("#output").text("Roman numerals don't go that high!")
+    } else if (userInput.length === 1) {
+      $("#output").text(numeralCodes[0][userInput]);
+    } else if (userInput.length === 2) {
+      $("#output").text(numeralCodes[1][userInput[0]] + numeralCodes[0][userInput[1]]);
+    } else if (userInput.length === 3) {
+      $("#output").text(numeralCodes[2][userInput[0]] + numeralCodes[1][userInput[1]] + numeralCodes[0][userInput[2]]);
+    } else if (userInput.length === 4) {
+      $("#output").text(numeralCodes[3][userInput[0]] + numeralCodes[2][userInput[1]] + numeralCodes[1][userInput[2]]+ numeralCodes[0][userInput[3]]);
+    }
+
 
   });
 });
